63508a149ce77fb1be91a9cfc22d1bdbc2d78f8f
e <425895811@qq.com>
2019-02-18 15:09:32 +0800

    练习react

---
+++ interface/interface/home/category.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        img: 'http://47.100.98.54:6000/category/1.png',
+        title: '美食'
+    },
+    {
+        img: 'http://47.100.98.54:6000/category/2.png',
+        title: '甜品饮品'
+    }
+]
+
+module.exports = data
